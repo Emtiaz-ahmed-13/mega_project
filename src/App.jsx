67b2920a9859e0cabc52a148bef68e36be35efba
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-09-01 15:09:34 +0600

    added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+
+function App() {
+  console.log(import.meta.env.REACT_APPWRITE_URL);
+  return (
+    <>
+    <h1>
+      blog app with appwrite
+    </h1>
+    </>
+  )
+}
+
+export default App
