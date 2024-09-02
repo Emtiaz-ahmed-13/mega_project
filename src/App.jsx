59084a88a3ba7764bb40c49c2517f84833d6329f
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-09-03 01:28:24 +0600

    till pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
+import authService from './appwrite/auth';
+import { Footer, Header } from "./components";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const userData = await authService.getCurrentUser();
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        dispatch(logout());
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkUser();
+  }, [dispatch]);
+
+  if (loading) {
+    return <div className='min-h-screen flex items-center justify-center'>Loading...</div>;
+  }
+
   return (
-    <>
-      <h1>
-        Blog app with Appwrite
-      </h1>
-    </>
+    <div className='min-h-screen flex flex-col bg-gray-400'>
+      <Header />
+      <main className='flex-grow'>
+        {/* <Outlet /> */}
+      </main>
+      <Footer />
+    </div>
   );
 }
 
