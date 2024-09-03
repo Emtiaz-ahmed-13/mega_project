0b9d7301a3c5c1ff5e079f7a747ecdb390408db7
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-09-03 23:18:51 +0600

    complete production grade components

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+
+import PropTypes from 'prop-types';
+
+function Logo({ width = "100px" }) {
+  return (
+    <div style={{ width }}>Logo</div>
+  );
+}
+
+Logo.propTypes = {
+  width: PropTypes.string
+};
+
+export default Logo;
