a2a5a6cbb0322cfed4c681345d7ff1d44297906a
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-09-05 01:02:13 +0600

    done with login

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import appwriteService from "../appwrite/config";
+
+
+//$id is a appwrite id ...
+function PostCard({ $id, title, featuredImage }) {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className='w-full bg-gray-100 rounded-xl p-4'>
+        <div className='w-full justify-center mb-4'>
+          <img
+            src={appwriteService.getFilePreview(featuredImage)}
+            alt={title}
+            className='rounded-xl'
+          />
+        </div>
+        <h2 className='text-xl font-bold'>{title}</h2>
+      </div>
+    </Link>
+  );
+}
+
+PostCard.propTypes = {
+  $id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  featuredImage: PropTypes.string.isRequired,
+};
+
+export default PostCard;
